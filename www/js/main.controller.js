ad574c2834ce0c7a1a7790c0a07d4355c70272f2
czl33 <519809610@qq.com>
2017-10-17 00:08:14 +0800

    优化，登陆，退出操作
    优化，登陆，退出操作

--- www/js/main.controller.js
+++ www/js/main.controller.js
@@ -1,19 +1,19 @@
-/**
- * Created by Administrator on 2017/9/17.
- */
 (function () {
   'use strict';
   angular.module('starter.controllers')
     .controller('MainCtrl',['$scope','localStorageService','$state',function ($scope,localStorageService,$state) {
       var app = localStorageService.get('App',{version:'1.0.0',run:false});
+      var User = localStorageService.get('user')
       if(app.run===false){
         app.run=true;
         localStorageService.update('App',app);
         $state.go('welcome');
       }
       else{
-        $state.go('app.home');
+        $state.go('login');
+                 if(User.isLogin==true){
+                        $state.go('app.home');
+                      }
       }
     }]);
 })();
-
